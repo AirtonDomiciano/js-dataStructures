a61e917de84462dfe55876a56bcfeb240b96a491
Airton <airton_mio@hotmail.com>
2021-05-06 21:35:33 -0300

    ajuste no sidebar

---
+++ src/section/chapter01-02/01-HelloWorld.js
@@ -0,0 +1,11 @@
+// @ts-check
+/* eslint-disable */
+
+function output(t) {
+  document.write('<p>' + t + '</p>');
+}
+
+alert('Airtão Projetos!!');
+console.log('Hello, Nois!');
+console.error('Hello, Error!');
+output('Hello, Venha conferir minha Pagina!');
